aa7eaecfa33f14e0aa529734c33d256fcd91ad68
El_makso <El_makso@mail.ru>
2022-04-15 19:42:27 +0300

    Add Example

---
+++ DevEduEducationSystem.API.Tests/StepDefinitions/RegistrationStepDefinitions.cs
@@ -0,0 +1,27 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace DevEduEducationSystem.API.Tests.StepDefinitions
+{
+    [Binding]
+    public class RegistrationStepDefinitions
+    {
+        [Given(@"Create registration model")]
+        public void GivenCreateRegistrationModel()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Activete registration endpoint")]
+        public void WhenActiveteRegistrationEndpoint()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"Should return CREATED response")]
+        public void ThenShouldReturnCREATEDResponse()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
